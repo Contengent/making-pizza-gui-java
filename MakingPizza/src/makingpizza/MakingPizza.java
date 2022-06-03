65e2352a6c77bc7ff0c6a2d8019ecc124a6baca4
Contengent <103345406+Contengent@users.noreply.github.com>
2022-06-03 00:27:18 -0400

    Add files via upload

---
+++ MakingPizza/src/makingpizza/MakingPizza.java
@@ -0,0 +1,20 @@
+package makingpizza;
+
+/**
+ * @author 
+ *
+ *   /\_/\  _     ___
+ *  |>'v'<||"| -/\, ,`>
+ *     -=Edelstein=-
+ * 
+ */
+public class MakingPizza {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
